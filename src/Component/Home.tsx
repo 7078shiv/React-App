d3226b944a72d8fedf59c3e78ae481078cb9754e
7078shiv <shivangshrivastava7078@gmail.com>
2023-05-20 19:49:57 +0530

    modified

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -4,13 +4,13 @@ const navigate=useNavigate();
 return(
     <>
     <h1> This is Home Page</h1>
-    <button onClick={()=>navigate('/Obama')}>Obama Page</button>
-    <button onClick={()=>navigate('/AddParams')}>AddParams Page</button>
-    <button onClick={()=>navigate('/Carousel')}>Carousel Page</button>
-    <button onClick={()=>navigate('/Donald')}>Donald Page</button>
-    <button onClick={()=>navigate('/Form')}>Form Page</button>
-    <button onClick={()=>navigate('/NoteTaking')}>NoteTaking</button>
-    <button onClick={()=>navigate('/Sachin')}>Sachin Page</button>
+    <button className="btn" onClick={()=>navigate('/Obama')}>Obama Page</button>
+    <button className="btn" onClick={()=>navigate('/AddParams')}>AddParams Page</button>
+    <button className="btn" onClick={()=>navigate('/Carousel')}>Carousel Page</button>
+    <button className="btn" onClick={()=>navigate('/Donald')}>Donald Page</button>
+    <button className="btn" onClick={()=>navigate('/Form')}>Form Page</button>
+    <button className="btn" onClick={()=>navigate('/NoteTaking')}>NoteTaking</button>
+    <button className="btn" onClick={()=>navigate('/Sachin')}>Sachin Page</button>
     </>
 )
 } 
