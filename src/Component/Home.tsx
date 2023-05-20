b16b58ac26a5f1f553ccda7dc420c5a738cf7165
7078shiv <shivangshrivastava7078@gmail.com>
2023-05-20 17:44:21 +0530

    modified

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,7 +1,17 @@
-
-
+import { useNavigate } from "react-router-dom";
 export default function Home(){
+const navigate=useNavigate();
 return(
+    <>
     <h1> This is Home Page</h1>
+    <h1> This is Donald Page</h1>
+    <button onClick={()=>navigate('/Obama')}>Obama Page</button>
+    <button onClick={()=>navigate('/AddParams')}>AddParams Page</button>
+    <button onClick={()=>navigate('/Carousel')}>Carousel Page</button>
+    <button onClick={()=>navigate('/Donald')}>Donald Page</button>
+    <button onClick={()=>navigate('/Form')}>Form Page</button>
+    <button onClick={()=>navigate('/NoteTaking')}>NoteTaking</button>
+    <button onClick={()=>navigate('/Sachin')}>Sachin Page</button>
+    </>
 )
 } 
